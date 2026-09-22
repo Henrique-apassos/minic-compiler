ae7bb9625c6d9c314aa359084599c22cd3a08197
Henrique <henriquepassos2004@outlook.com.br>
2026-09-22 20:16:23 -0300

    fix: organiza estrutura de pastas na raiz e atualiza Main.lean

---
+++ Main.lean
@@ -0,0 +1,5 @@
+import Scanner
+import Automata.DFA
+
+def main : IO Unit := do
+  IO.println "Compilador Mini-C pronto para execução."
--- Scanner/Main.lean
+++
@@ -1,4 +0,0 @@
-import Scanner
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
